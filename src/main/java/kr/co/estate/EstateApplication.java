805f46b00bd772e96d9d840d5ed1018cd89a3722
Imchaehoon <youspend8@gmail.com>
2020-12-10 21:54:59 +0900

    remove : SpringBootServletInitializer 제거

--- src/main/java/kr/co/estate/EstateApplication.java
+++ src/main/java/kr/co/estate/EstateApplication.java
@@ -2,18 +2,12 @@ package kr.co.estate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EstateApplication extends SpringBootServletInitializer {
+public class EstateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EstateApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(EstateApplication.class);
-    }
 }
